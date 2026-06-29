8e3a8ddf838dc923a418b74929579535fa02954d
Karthik <210724196+karthik-kay@users.noreply.github.com>
2026-06-29 21:00:16 +0530

    Refactor settings.py to use decouple for config

--- backend/django-service/django_service/settings.py
+++ backend/django-service/django_service/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -109,12 +111,12 @@ ORG_CEO_TITLE      = "Chief Executive Officer"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-from decouple import config
+
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-import dj_database_url
+
 
 DATABASES = {
     "default": dj_database_url.config(
